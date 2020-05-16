b113dbc4ef12eb45f1a21401b07d0beeecd57e4e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-17 01:59:10 +0800

    equality and relational fundamentals

---
+++ basic/equality_relational_operators.dart
@@ -0,0 +1,11 @@
+// this is a simple application just to show the equality and relational operators in Dart
+main() {
+  int ageRestricted = 18;
+
+  print(ageRestricted == 10);
+  print(ageRestricted != 10);
+  print(ageRestricted > 10);
+  print(ageRestricted < 10);
+  print(ageRestricted >= 10);
+  print(ageRestricted <= 10);
+}
