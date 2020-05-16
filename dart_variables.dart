9a3f5f421b344ff118b4a1a2cf9096833cdbce57
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-17 00:20:21 +0800

    dart tutorials

---
+++ dart_variables.dart
@@ -0,0 +1,9 @@
+main() {
+  // variables
+
+  /* both name and country_origin are Strings */
+  var name = "Ahmad Irfan";
+  var country_origin = "Kelantan";
+
+  print("My name is " + name + " and I am from " + country_origin);
+}
---
+++ variables_type.dart
@@ -0,0 +1,24 @@
+main() {
+  /*
+    Type Variable in Dart are same as in Java (or any other programming languages such as C)
+    1.) String
+    2.) Number
+    3.) Boolean
+    4.) lists
+    5.) maps
+    6.) etc...
+  */
+  var country;
+  country = "Japan";
+  country = 2;
+
+  String name; // explicit variables where we can only String that contains String
+
+  name = "Ahmad Irfan";
+
+  num age = 32;
+
+  print(name);
+  print(country);
+  print(age);
+}
