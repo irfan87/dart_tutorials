49651e5f53898064a5c2b84018b2e2c3b26b688e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-17 00:44:45 +0800

    learned what are const and final in Dart, and when to use it. More depth will be next

---
+++ basic/const_and_final.dart
@@ -0,0 +1,14 @@
+main() {
+  // const and final keywords
+
+  /*
+    const is the keyword where we wan to use as a constant for value 
+    final - if we want a value/variable to always be constant and it can not change (or mutable)
+  */
+
+  const pi = 3.14;
+  final earthWeight = 3.1231;
+
+  print(pi);
+  print(earthWeight);
+}
