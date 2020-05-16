c90f011f0fbaf380741b4e0e7e6416aa031783d4
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-17 02:11:58 +0800

    learn the basic of if else statement

---
+++ control_flow_and_functions/dart_if_statements.dart
@@ -0,0 +1,15 @@
+main() {
+  int myNumber = 14;
+  
+  // print(myNumber is! double);
+  
+  if(myNumber == 15) {
+    print('It is');
+  } else if (myNumber != 15) {
+    print('It is not');
+  } else {
+    print('Default');
+  }
+
+  // myNumber == 15 ? print('it is') : print('it is not');
+}
