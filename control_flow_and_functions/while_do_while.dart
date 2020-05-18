0d5ad453e36208e38aaa76b7434fea3cbd4fc1c9
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-18 16:30:15 +0800

    playaround with while and do..while

---
+++ control_flow_and_functions/while_do_while.dart
@@ -0,0 +1,15 @@
+main() {
+
+  int myNumber = 15;
+
+  while(true) {
+    if(myNumber >= 15) {
+      print("Going");
+      break;
+    }
+  }
+
+  // do {
+  //   print("Testing");
+  // } while(myNumber < 30);
+}
