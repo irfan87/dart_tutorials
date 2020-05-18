6b41ab0e5e85da10aafce4d1333db3c89c65cf04
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-18 20:04:50 +0800

    playaround arrow functions

---
+++ control_flow_and_functions/functions/arrow_functions.dart
@@ -0,0 +1,10 @@
+main() {
+  String myName = getName();
+  int myAge = getAge();
+
+  print("$myName is my name and I am $myAge");
+}
+
+String getName() => 'James Bond'; // return expressions.. same like es6.. =)
+
+int getAge() => 32;
