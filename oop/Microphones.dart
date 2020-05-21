0b1978cbc399d9275b172cae56fdc0afa18411b9
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-22 00:22:10 +0800

    add methods

--- oop/Microphones.dart
+++ oop/Microphones.dart
@@ -6,15 +6,34 @@ class Microphones {
   String color;
   int model = 4312;
   double watt;
+
+  void turnOn() {
+    print("${name} is turn on");
+  }
+
+  void turnOff() {
+    print("${name} is turn off");
+  }
+
+  void setVolume() {
+    print("${name} with colors: ${color} volume is up");
+  }
+
+  bool isOn() => true;
+  int modelNumber() => model;
 }
 
 main() {
   var mic = Microphones();
-  
+
   mic.name = 'Audio Technica';
   mic.color = 'black';
   mic.model = 1231;
   mic.watt = 2.2;
+  
+  mic.turnOn();
+  mic.setVolume();
+  mic.turnOff();
 
-  print("Item name: ${mic.name}\nColor: ${mic.color}\nModel: ${mic.model}\nPower: ${mic.watt} watt"); 
+  print("\nItem name: ${mic.name}\nColor: ${mic.color}\nModel: ${mic.modelNumber()}\nPower: ${mic.watt} watt\nOn?: ${mic.isOn()}"); 
 }
